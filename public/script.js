51a55a138b5a75c0a1aad5684b7058c36b236523
gbeltranl <g.beltranl@uniandes.edu.co>
2022-03-14 18:56:26 -0500

    finishh

--- public/script.js
+++ public/script.js
@@ -4,10 +4,25 @@ const table = document.getElementById('table');
 const tbody = document.getElementById('body-table');
 const message = document.getElementById('message');
 
+function render(usr){
+  message.innerText = usr.username;
+  const tracks = document.createElement('div');
+      table.appendChild(tracks);
+    usr.tracks.forEach(element => {
+      let cont = `<tr>
+      ${element}
+      </tr>`
+      tbody.innerHTML += cont;
+    });
+
+  
+
+}
 enterButton.addEventListener('click', (event) => {
   //Implementar lógica del button submit
-  alert('Implementar lógica del button submit');
-  getUser('james');
+  //console.log(event);
+  //console.log(input.value);
+  const data = getUser(input.value);
   event.preventDefault();
 });
 
@@ -16,9 +31,11 @@ enterButton.addEventListener('click', (event) => {
  * @param {*} username
  */
 async function getUser(username) {
+  //console.log(username);
   const resp = await fetch(`api/users/${username}`);
   const data = await resp.json();
   console.log('data from back', data);
+  render(JSON.parse(data));
   //printValues(data);
 }
 
--- src/controller.js
+++ src/controller.js
@@ -1,6 +1,24 @@
-const getUser = async () => {
+const axios = require('axios');
+const getUser = async (username) => {
   //TODO user logic
-  return { message: 'To be implemented' };
+  var usr = new Object();
+  usr.username = username;
+  await axios.get(` https://mauvelous-leopard-5257.twil.io/plays-detail?username=${username}`)
+  .then(function (response) {
+    usr.plays =new Set(response.data.plays).size;
+    usr.tracks = response.data.plays.filter((v, i, a) => a.indexOf(v) === i);
+  });
+  await axios.get(`https://mauvelous-leopard-5257.twil.io/friend-detail?username=${username}`)
+  .then(function (response) {
+    // handle success
+    usr.friends = new Set(response.data.friends).size;
+  });
+  usr.uri = "users/" + username;
+  return JSON.stringify(usr) ;
+  
+  
+
+  //return { message: 'To be implemented' };
 };
 
 module.exports = { getUser };
